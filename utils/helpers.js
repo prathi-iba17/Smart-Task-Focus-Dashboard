f43e5c285f522544223f84d31a389f54f8c716d2
prathi-iba17 <prathi.iba.17@gmail.com>
2026-01-02 20:26:04 +0530

    Delete utils directory

--- utils/helpers.js
+++
@@ -1,8 +0,0 @@
-export const sortTasks = (tasks, sortBy) => {
-return [...tasks].sort((a, b) => {
-if (sortBy === 'due') return new Date(a.dueDate) - new Date(b.dueDate)
-if (sortBy === 'created') return new Date(a.createdAt) - new Date(b.createdAt)
-if (sortBy === 'priority') return b.priority - a.priority
-return 0
-})
-}

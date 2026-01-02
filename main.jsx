2c042390c39e0b3a9ed3a5a16b08361ded64ce0c
prathi-iba17 <prathi.iba.17@gmail.com>
2026-01-02 20:24:08 +0530

    Delete main.jsx

--- main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import App from "./App"
-import ReactDOM from "react-dom/client"
-import "./styles/index.css"
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-<App />
-)
